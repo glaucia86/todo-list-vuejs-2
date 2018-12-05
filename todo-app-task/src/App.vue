6539df59ddb1233b120a5d06acb363c89e4cb477
Glaucia Lemos <glaucia86>
2018-12-04 22:55:24 -0200

    Testando o código desenvolvido.

--- todo-app-task/src/App.vue
+++ todo-app-task/src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
+    <todo-list></todo-list>
     <router-view/>
   </div>
 </template>
 
-<script>
+<script src='./services/Data.js'>
 
-import TodoList from '@/components/pages/TodoList';
+/* import TodoList from '@/components/pages/TodoList';
 
 export default {
   name: 'app',
   components: {
-
+    TodoList,
   },
   data() {
     return {
       msg: '',
     };
   },
-};
+}; */
 </script>
 
 <style></style>
