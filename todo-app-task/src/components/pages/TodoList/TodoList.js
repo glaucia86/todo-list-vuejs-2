134dc89303ab8496547e9145fc769658db507b0f
Glaucia Lemos <glaucia86>
2018-12-15 12:57:41 -0200

    commit

--- todo-app-task/src/components/pages/TodoList/TodoList.js
+++ todo-app-task/src/components/pages/TodoList/TodoList.js
@@ -1,9 +1,10 @@
 /**
+ *
  * Arquivo: src/components/pages/TodoList/TodoList.js
  * Data: 04/12/2018
- * Descrição: arquivo responsável por lidar com a lógica inerente ao componente
- *  'TodoList'
+ * Descrição: arquivo responsável por lidar com a lógica inerente ao componente 'TodoList'
  * Autora: Glaucia Lemos
+ *
  */
 
 export default {
