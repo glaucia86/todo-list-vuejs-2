e9910e3dbfda3ba84e0324ddbe651678eda1bf51
Glaucia Lemos <glaucia86>
2018-12-15 12:52:15 -0200

    commit

--- todo-app-task/src/components/pages/Todo/Todo.js
+++ todo-app-task/src/components/pages/Todo/Todo.js
@@ -1,9 +1,10 @@
 /**
+ *
  * Arquivo: src/components/pages/Todo/Todo.js
  * Data: 13/12/2018
- * Descrição: arquivo responsável por lidar com a lógica inerente ao componente
- *  'Todo'
+ * Descrição: arquivo responsável por lidar com a lógica inerente ao componente 'Todo'.
  * Autora: Glaucia Lemos
+ *
  */
 
 export default {
