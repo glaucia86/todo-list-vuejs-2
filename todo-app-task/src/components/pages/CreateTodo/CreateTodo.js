25eaf773cffdfc06f2b624ec784b617d05a2405e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-25 15:16:40 -0200

    Início desenvolvimento da lógica inerente ao componente 'CreateTodo'

---
+++ todo-app-task/src/components/pages/CreateTodo/CreateTodo.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Arquivo: src/components/pages/CreateTodo/CreateTodo.js
+ * Data: 25/12/2018
+ * Descrição: arquivo responsável por lidar com a lógica inerente ao componente 'CreateTodo'.
+ * Autora: Glaucia Lemos
+ *
+ */
+
+export default {
+  name: 'CreateTodo',
+  data() {
+    return {
+      msg: 'Componente CreateTodo',
+    };
+  },
+};
---
+++ todo-app-task/src/components/pages/CreateTodo/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ui basic content center aligned segment">
+    <button class="ui basic button icon" v-on:click="abrirForm" v-show="!criarTodo">
+      <i class="plus icon"></i>
+    </button>
+    <div class="ui centered card" v-show="criarTodo">
+      <div class="content">
+        <div class="ui form">
+          <div class="field">
+            <label>Título</label>
+            <input v-model="titulo" type="text" ref="titulo" defaultValue="">
+          </div>
+          <div class="field">
+            <label>Projeto</label>
+            <input type="text" ref="projeto" defaultValue="">
+          </div>
+          <div class="ui two button attached buttons">
+            <button class="ui basic blue button" v-on:click="enviarForm()">
+              Adicionar
+            </button>
+            <button class="ui basic red button" v-on:click="fecharForm">
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./CreateTodo" />
+<style scoped></style>
+
